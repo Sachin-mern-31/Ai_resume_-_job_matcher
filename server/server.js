66d3e2237a80078ce4ad3c3acb60ea3002ac8f28
Sachin-mern-31 <mernstack37382@gmail.com>
2026-09-13 21:26:58 +0530

    chore(config): update production render backend target in vite.config.js and add vercel domain to CORS allowedOrigins

--- server/server.js
+++ server/server.js
@@ -12,6 +12,7 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://127.0.0.1:5173',
   'http://localhost:3000',
+  'https://resumematchwithsachin.vercel.app',
   process.env.CLIENT_URL,
 ].filter(Boolean);
 
